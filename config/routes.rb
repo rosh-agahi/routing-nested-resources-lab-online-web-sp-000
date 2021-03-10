b2baf0ac3a87015534088867763048e87acf56bd
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-10 05:02:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists
+  
+    resources :artists, only: [:index, :show] do
+      resources :songs, only: [:index, :show]
+    end
+  
   resources :songs
+
 end
